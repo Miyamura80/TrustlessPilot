881d42e8c1b09141297d15b71fb5085468658dda
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2022-10-08 16:20:29 -0500

    start of view product page

---
+++ marketplace_demo/pages/ViewProductPage.tsx
@@ -0,0 +1,11 @@
+import Carousel from "../components/Carousel";
+import { Page } from "../components/Page";
+import MinterPage from "./MinterPage";
+
+export default function ViewProductPage() {
+  return (
+    <Page>
+      <Carousel />
+    </Page>
+  );
+}
