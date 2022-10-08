dda9f1ad95dc6b9ccc403145736fbac6d461e939
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2022-10-08 16:02:11 -0500

    other components

--- marketplace_demo/pages/MarketplacePage.tsx
+++ marketplace_demo/pages/MarketplacePage.tsx
@@ -1,5 +1,7 @@
+import Carousel from "../components/Carousel";
 import Marketplace from "../components/Marketplace";
 import { Page } from "../components/Page";
+import MinterPage from "./MinterPage";
 
 export default function MarketplacePage() {
   return (
