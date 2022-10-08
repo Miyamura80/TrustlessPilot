c31a5fc54d382cf6ee839b9806494f7f5499d249
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-08 13:59:55 -0500

    bugfixing ipfs

--- backend/routes/createReview.js
+++ backend/routes/createReview.js
@@ -5,13 +5,22 @@ const { createReview } = require('../components/contract/createReview')
 
 router.post('/', async function(req, res, next) {
   const { title, score, content, author, reviewedProductId, chainId, contractAddress, tokenId } = req.body;
-  const postRes = await postToIPFS({
+
+  var metadataUri = null;
+  const postRes = postToIPFS.resolve({
     title: title,
     score: score,
     content: content
-  });
-  const metadataUri = ""; // get from postRes
-  await createReview(chainId, contractAddress, tokenId, metadataUri, author)
+  }).then( (res) => {
+    metadataUri = res.Hash;
+    // console.log(res);
+    // res -> name, hash, size
+  }).then( () => {
+    console.log(metadataUri)
+    createReview(chainId, contractAddress, tokenId, metadataUri, author);
+  })
+  .catch((err) => console.error(err));
+
   res.status(200).json({ message: "Review created!"});
 });
 
