dc5a7d4ec66fc104c7e45ab7ddcc5c2078c24ae2
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2022-10-08 16:20:36 -0500

    change shit

--- marketplace_demo/pages/ViewProductPage.tsx
+++ marketplace_demo/pages/ViewProductPage.tsx
@@ -2,10 +2,8 @@ import Carousel from "../components/Carousel";
 import { Page } from "../components/Page";
 import MinterPage from "./MinterPage";
 
-export default function ViewProductPage() {
+export default function ViewProductPage({ price,tokenId,seller,owner,image,name,description }: any) {
   return (
-    <Page>
-      <Carousel />
-    </Page>
+    <></>
   );
 }
