633c8505c7fd28839930982a801f3895ff1b11d6
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-09 07:16:21 -0500

    deleted useless import

--- marketplace_demo/pages/product/[...slug].tsx
+++ marketplace_demo/pages/product/[...slug].tsx
@@ -1,5 +1,4 @@
 import { Page } from '../../components/Page';
-import { useProductContext } from '../_app';
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from 'next/link';
