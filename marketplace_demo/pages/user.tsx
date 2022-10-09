2c1f09520ed833bbd6863d311636171760757fff
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-09 06:35:42 -0500

    fix user page reviewContainer

--- marketplace_demo/pages/user.tsx
+++ marketplace_demo/pages/user.tsx
@@ -6,13 +6,10 @@ import { FaVoteYea } from "react-icons/fa";
 import { BsPencilSquare } from "react-icons/bs";
 import Voting from "../components/review/Voting";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { formatAddress } from '../utils/formatting';
 import { WorldcoinWidget } from '../components/profile'
 import { useRouter } from 'next/router'
-=======
 import ReviewContainer from "../components/review/ReviewContainer";
->>>>>>> 155a308de0a602750a6f4f6cb2c0e1e93bb9fc0d
 
 export default function UserPage() {
   const router = useRouter();
@@ -20,7 +17,9 @@ export default function UserPage() {
   const verifiedHuman = true;
   const globalScore = 2000;
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isReady, setIsReady] = useState(router.isReady)
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasMounted, setHasMounted] = useState(false)
 
   const [data, setData] = useState(null);
   const [activeTab, setActiveTab] = useState('reviews')
@@ -34,9 +33,21 @@ export default function UserPage() {
       })
       .then(() => setIsLoading(false))
       .catch((error) => console.log(error));
-  }, []);
+  }, [])
 
-  if (isLoading) return <div>loading</div>;
+  useEffect(() => {
+    setIsReady(router.isReady)
+  }, [router])
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+  if (!hasMounted) {
+    return null
+  }
+
+  if (isLoading || !isReady ) return <div>loading</div>;
+  console.log("router ", router.query)
   console.log("data ", data);
   const reviews = data.userReviews;
   const profileData = data.profile.profiles[0];
@@ -107,7 +118,10 @@ export default function UserPage() {
             </h4>
           </div>
           <div className="flex flex-col justify-center my-4">
-            {activeTab == 'reviews' ? displayReviews : null}
+            { (activeTab == 'reviews' )
+            ? <ReviewContainer data={reviews} />
+            : <></>
+            }
           </div>
         </div>
       </div>
