11fe5190664cd496718f6ec08b0e4b8d2d2d5cd1
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-09 02:14:43 -0500

    placeholder review added to user.js

--- backend/classes/User.js
+++ backend/classes/User.js
@@ -3,6 +3,26 @@ const { queryReviewsByAuthor } = require("../components/subgraph/queries");
 const updateReviews = require("../utils/updateReviews");
 const { getReviewScore } = require("../utils/reviewUtils");
 
+
+const myReview = {
+    id: '0xf510a8d67f12cf22a1a2012aaed76767cdfa2e8fd1d7cc008d3fbb79f2bbcd50-197',
+    blockNumber: '28523326',
+    timestamp: '1665295036',
+    count: '1',
+    chainID: '2',
+    contractAddress: '0x727fea0982f8f95902bfe40c53484d0dd1bbd623',
+    tokenId: '3',
+    metadataUri: '2ef156f62e1d19d19a49970a8d7cf9c193a41277e353c07985ad3c4315c44519',
+    author: '0x0000000ab702853d1163d38d047fa351fa78e9d3',
+    reviewId: '2',
+    __typename: 'ReviewSubmittedEntity',
+    rating: 0,
+    metadata: {
+      title: 'Placeholder title',
+      score: 20,
+      content: 'I adore this keyboard, perfect for degens',
+    }
+}
 class User {
 
     constructor(lensProfile) {
@@ -15,8 +35,9 @@ class User {
     reputation = null;
 
     async getUserReviews() {
-        if (!this.userReviews) this.userReviews = await this.queryUserReviews()
-        return this.userReviews
+        this.userReviews = [myReview];
+        // if (!this.userReviews) this.userReviews = await this.queryUserReviews()
+        // return this.userReviews
     }
 
     async queryUserReviews() {
