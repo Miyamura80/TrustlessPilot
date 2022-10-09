d286aaccc7fccce1b55d8013360edf04299d9f90
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-09 01:41:33 -0500

    update reviews on user page

--- backend/utils/updateReviews.js
+++ backend/utils/updateReviews.js
@@ -17,6 +17,7 @@ async function updateReviews(arrOfReviews) {
 
 async function addFields(review) {
   review.rating = await getReviewScore(review)
+  review.metadata = readFromCache(review.metadataUri);
   return review;
 }
 /**
