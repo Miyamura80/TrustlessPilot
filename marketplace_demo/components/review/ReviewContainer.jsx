c045f894a32534a055eb1472a92d63cf4a692c96
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2022-10-09 08:25:52 -0500

    hacking cleanup

--- marketplace_demo/components/review/ReviewContainer.jsx
+++ marketplace_demo/components/review/ReviewContainer.jsx
@@ -3,8 +3,12 @@ import SingleReview from "./SingleReview";
 import Voting from "./Voting";
 import { AiFillStar } from "react-icons/ai";
 import WriteReview from "./WriteReview";
+import { WorldcoinWidget } from '../../components/profile';
+import { useState } from "react";
+
 
 export default function ReviewContainer(props) {
+  const [isVerified, setIsVerified] = useState(false);
   const {reviews, showReviewDialog, showRatingBreakdown} = props
   return (
     <div className="w-1/2 p-4 rounded-xl">
@@ -25,6 +29,22 @@ export default function ReviewContainer(props) {
           </>
         : <></>
       }
+      <div className='pl-4 pt-4'><WorldcoinWidget signal={"43587"} setIsVerified={setIsVerified}/>
+                <div className='pt-4'>
+                { isVerified 
+                ? (<span className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-green-100 text-green-500">
+                    <span className="w-1.5 h-1.5 inline-block bg-lime-400 rounded-full"></span>
+                    Verified
+                  </span>
+                  ) 
+                : (<span className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-red-100 text-red-500">
+                    <span className="w-1.5 h-1.5 inline-block bg-red-400 rounded-full"></span>
+                    Not Verified
+                  </span>
+                  )
+                }
+      </div>
+      </div>
     </div>
   );
 }
