a39dbc2a384b98c28ff3c9c167df99f6977325dd
Amalia Rebegea <amaliareb@yahoo.com>
2022-10-09 02:18:48 -0500

    bugfix

--- backend/classes/User.js
+++ backend/classes/User.js
@@ -4,7 +4,7 @@ const updateReviews = require("../utils/updateReviews");
 const { getReviewScore } = require("../utils/reviewUtils");
 
 
-const myReview = {
+const myReviews = [{
     id: '0xf510a8d67f12cf22a1a2012aaed76767cdfa2e8fd1d7cc008d3fbb79f2bbcd50-197',
     blockNumber: '28523326',
     timestamp: '1665295036',
@@ -18,11 +18,31 @@ const myReview = {
     __typename: 'ReviewSubmittedEntity',
     rating: 0,
     metadata: {
-      title: 'Placeholder title',
+      title: 'Placeholder title 1',
       score: 20,
       content: 'I adore this keyboard, perfect for degens',
     }
-}
+    },
+    {
+    id: '0xf510a8d67f12cf22a1a2012aaed76767cdfa2e8fd1d7cc008d3fbb79f2bbcd50-197',
+    blockNumber: '28523326',
+    timestamp: '1665295036',
+    count: '1',
+    chainID: '2',
+    contractAddress: '0x727fea0982f8f95902bfe40c53484d0dd1bbd623',
+    tokenId: '3',
+    metadataUri: '2ef156f62e1d19d19a49970a8d7cf9c193a41277e353c07985ad3c4315c44519',
+    author: '0x0000000ab702853d1163d38d047fa351fa78e9d3',
+    reviewId: '2',
+    __typename: 'ReviewSubmittedEntity',
+    rating: 0,
+    metadata: {
+      title: 'Placeholder title 2',
+      score: 20,
+      content: 'I adore this keyboard, perfect for degens',
+    }
+    }
+]
 class User {
 
     constructor(lensProfile) {
@@ -35,9 +55,9 @@ class User {
     reputation = null;
 
     async getUserReviews() {
-        this.userReviews = [myReview];
+        this.userReviews = myReviews;
         // if (!this.userReviews) this.userReviews = await this.queryUserReviews()
-        // return this.userReviews
+         return this.userReviews
     }
 
     async queryUserReviews() {
