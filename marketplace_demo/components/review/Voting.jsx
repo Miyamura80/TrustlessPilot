7d66158808e5bbf4910fa5f07000f4980bf802c6
sethhowes <71157822+sethhowes@users.noreply.github.com>
2022-10-08 22:24:11 -0500

    user page updated

--- marketplace_demo/components/review/Voting.jsx
+++ marketplace_demo/components/review/Voting.jsx
@@ -2,17 +2,20 @@ import { VscChevronUp, VscChevronDown } from "react-icons/vsc";
 
 
 export default function Voting() {
-
+    const totalVotes = 128;
     return (
         <div>
             <div>
                 <button className="hover:bg-emerald-200 hover:rounded-xl">
-                    <VscChevronUp size="28" />
+                    <VscChevronUp size="24" />
                 </button>
+            </div>
+            <div className="mb-1 text-sm">
+                {totalVotes}
             </div>
             <div>
                 <button className="hover:bg-red-300 hover:rounded-xl">
-                    <VscChevronDown size="28" />
+                    <VscChevronDown size="24" />
                 </button>
             </div>
         </div>
--- marketplace_demo/pages/UserPage.tsx
+++ marketplace_demo/pages/UserPage.tsx
@@ -2,7 +2,10 @@ import { Page } from "../components/Page";
 import ReviewText from "../components/review/ReviewText";
 import Image from "next/image";
 import { MdVerified } from "react-icons/md";
+import { FaVoteYea } from "react-icons/fa";
+import { BsPencilSquare } from "react-icons/bs";
 import { Tooltip, Tabs } from "flowbite-react";
+import Voting from "../components/review/Voting";
 
 export default function UserPage() {
   const walletAddress = "0xDfDeD3Eh4DFD409A489F5bH590f6fc9f314587E0";
@@ -42,23 +45,42 @@ export default function UserPage() {
           </div>
           <div className="flex">
             <div className="flex-1">
-              <p>Followers</p>
               <p>{followers}</p>
+              <p className="text-sm text-slate-500">Followers</p>
             </div>
             <div className="flex-1">
-              <p>Following</p>
               <p>{following}</p>
+              <p className="text-sm text-slate-500">Following</p>
             </div>
           </div>
         </div>
         <Tabs.Group aria-label="Default tabs" style="default">
-          <Tabs.Item active={true} title="Reviews Written">
-            <div className="basis-3/5 flex-none border-2 rounded-xl p-6 m-8">
-              <h1 className="text-3xl mb-5">NFT image</h1>
-              <ReviewText />
+          <Tabs.Item
+            active={true}
+            title="Reviews Written"
+            icon={BsPencilSquare}
+          >
+            <div className="basis-3/5 flex-none flex border-2 rounded-xl p-6 m-4">
+              <div className="flex-auto mr-5">
+                <Voting />
+              </div>
+              <div className="flex-auto">
+                <h1 className="text-3xl mb-5">NFT image</h1>
+                <ReviewText />
+              </div>
             </div>
           </Tabs.Item>
-          <Tabs.Item title="Reviews Rated">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Reviews Rated" icon={FaVoteYea}>
+            <div className="basis-3/5 flex-none flex border-2 rounded-xl p-6 m-4">
+              <div className="flex-auto mr-5">
+                <Voting />
+              </div>
+              <div className="flex-auto">
+                <h1 className="text-3xl mb-5">NFT image</h1>
+                <ReviewText />
+              </div>
+            </div>
+          </Tabs.Item>
         </Tabs.Group>
       </div>
     </Page>
